bc312a5a47889c57eb7c0aaa950f134aca5c760d
Carl Fürstenberg <carl@excito.com>
2010-07-19 16:05:49 +0000

    test messages

--- buildscript.sh
+++ buildscript.sh
@@ -20,7 +20,7 @@
 
 # die die
 set -e 
-set -x
+. /lib/lsb/init-functions
 
 ROOT="./root";
 INSTALL="./install"
@@ -32,7 +32,7 @@ if [ ! -d installer ]; then
 	if [ `basename $PWD` = 'installer' ]; then
 		cd ..
 	else
-		echo "need to be executed in topdir above installer dir";
+		log_failure_msg "need to be executed in topdir above installer dir";
 		exit 1;
 	fi;
 fi
@@ -46,29 +46,39 @@ rm -rf $INSTALL $ROOT
 # make the skeleton
 ( cd installer && make skel )
 if [ ! -d /usr/share/cdebootstrap/excito ]; then
-	DEBIAN_FRONTEND=noninteractive apt-get update
-	DEBIAN_FRONTEND=noninteractive apt-get install -y cdebootstrap-excito
+	log_action_begin_msg "installing cdebootstrap"
+	DEBIAN_FRONTEND=noninteractive apt-get update >/dev/null
+	DEBIAN_FRONTEND=noninteractive apt-get install -y cdebootstrap-excito >/dev/null
+	log_action_end_msg
 fi
 
 # the debootstrap
 cdebootstrap $SUITE $ROOT $URI
 
 # install the skeleton (XXX make obsolete?)
-tar -zxvf installer/skeleton.tar.gz -C $ROOT/
+log_action_begin_msg "installing skeleton"
+tar -zxf installer/skeleton.tar.gz -C $ROOT/
+log_action_end_msg
 
 # change hostname to bubba
 _old_hostname=`cat /proc/sys/kernel/hostname`;
+log_action_msg "setting hostname to \"bubba\""
 echo bubba > /proc/sys/kernel/hostname;
 
 #stop running mysql
 if [ -e /etc/init.d/mysql ]; then
+	log_action_begin_msg "terminating mysql"
 	/etc/init.d/mysql stop || true
+	log_action_end_msg
 fi
 
 # setup answers to debconf
+log_action_begin_msg "preseeding debconf"
 cat installer/debconf | chroot $ROOT debconf-set-selections
+log_action_end_msg
 
 # stage two build script
+log_action_msg "Entering stage 2 builder"
 cp installer/buildscript_stage2.sh $ROOT/runme.sh
 chmod 755 $ROOT/runme.sh
 
@@ -79,12 +89,15 @@ chroot $ROOT /runme.sh
 rm -f $ROOT/runme.sh
 
 # truncate all logs
+log_action_msg "truncating logs"
 for i in $(find $ROOT/var/log -type f); do :> $i; done
 
 # remove eventual pids
+log_action_msg "removing old pid files"
 find $ROOT/var/run/ -name "*.pid" -exec rm {} \;
 
 # truncate bash history
+log_action_msg "truncating root bash history"
 :> $ROOT/root/.bash_history
 
 # conf fixes
@@ -92,28 +105,42 @@ perl -pi -e 's/sysfs_scan = 1/sysfs_scan = 0/' $ROOT/etc/lvm/lvm.conf
 perl -pi -e 's/NO_START=1/NO_START=0/' $ROOT/etc/default/apache2
 
 # build the payload
+log_action_begin_msg "building payload"
 export _date=`date +%y%m%d-%H%M`
 ( cd $ROOT && tar --create --verbose --file ../bubbaroot-$_date.tar * )
+log_action_end_msg
+log_action_begin_msg "compressing payload"
 gzip --fast bubbaroot-$_date.tar
+log_action_end_msg
 
 # extract the envelope
+log_action_begin_msg "extracting envelope"
 tar -zxvf installer/envelope.tar.gz
+log_action_end_msg
 
 # move payload into envelope
+log_action_msg "moving payload into envelope"
 mv bubbaroot-$_date.tar.gz $INSTALL/payload
 
 # zip and checksum the install
 ver=${1:-0.0.1}
+log_action_begin_msg "generating checksums"
 zip -0 -r $FILENAME-$ver.zip $INSTALL
 sha1sum $FILENAME-$ver.zip > $FILENAME-$ver.zip.sha1
 sha256sum $FILENAME-$ver.zip > $FILENAME-$ver.zip.sha256
+log_action_end_msg
 
 #(re)start mysql
 if [ -e /etc/init.d/mysql ]; then
+	log_action_begin_msg "restarting mysql"
 	/etc/init.d/mysql start || true
+	log_action_end_msg
 fi
 
 # restore hostname
+log_action_msg "restoring hostname \"$_old_hostname\""
 echo $_old_hostname > /proc/sys/kernel/hostname;
 
+log_action_begin_msg "cleaning installer build dir"
 ( cd installer && make clean )
+log_action_end_msg
--- buildscript_stage2.sh
+++ buildscript_stage2.sh
@@ -17,39 +17,56 @@
 #       CREATED:  09/01/2009 11:08:29 AM CEST
 #      REVISION:  ---
 #===============================================================================
+set -e
+. /lib/lsb/init-functions
 
+log_action_msg "mouting proc"
 mount /proc;
 
+log_action_msg "setting up repositories"
 sed -i 's/unstable/testing/g;s/vincent/hugo/g' /etc/apt/preferences
 cat <<EOF > /etc/apt/sources.list
 deb http://b3.update.excito.org/ hugo main
 deb http://b3.update.excito.org/ upstream_squeeze_forhugo main
 EOF
 
-apt-get update
+log_action_begin_msg "updating repositories"
+apt-get update >/dev/null
+log_action_end_msg
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y postfix mysql-server
+log_action_begin_msg "installing postifx and mysql"
+DEBIAN_FRONTEND=noninteractive apt-get install -y postfix mysql-server >/dev/null
+log_action_end_msg
 
+log_action_msg "enabling policy-rc.d"
 cat <<EOF > /usr/sbin/policy-rc.d
 #!/bin/sh
 exit 101
 EOF
 chmod 755 /usr/sbin/policy-rc.d
 
-DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get install -y bubba3-kernel bubba3-modules bubba squeezecenter less
+log_action_begin_msg "installing bubba packages"
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get install -y bubba3-kernel bubba3-modules bubba squeezecenter less >/dev/null
+log_action_end_msg
 
+log_action_msg "disabling policy-rc.d"
 rm -f /usr/sbin/policy-rc.d
 
+log_action_msg "shutting down mysql"
 invoke-rc.d mysql stop
 
+log_action_msg "stting root password"
 usermod -p '$1$AzIyDBlb$x09YPfzWv11Bvvl9SzZA/1' root
 
+log_action_msg "enabling shadow"
 shadowconfig on
 
+log_action_msg "clean apt"
 apt-get clean
 
 rm -f /var/lib/apt/lists/*excito.org_dists_*
 
+log_action_msg "installing bubba apt config"
 cp /usr/share/bubba-configs/apt/* /etc/apt/
 
 ## TODO fixed?
@@ -57,6 +74,7 @@ chown root:users /home/storage
 chmod 1777 /home/storage
 
 
+log_action_msg "unmount proc"
 umount /proc
 
 exit
